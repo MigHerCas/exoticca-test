dbf397fbc626f460ee0abe9a69d4fd9aa7e6a10d
Miguel Hernanz <migherdcas@gmail.com>
2021-02-04 11:19:17 +0100

    Set-up.

---
+++ src/styled/index.ts
@@ -0,0 +1,49 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'Red Hat Text', sans-serif;
+    margin: 0;
+    padding: 0;
+    font-size: 62.5%; // 1rem -> 10px
+  }
+
+  html,
+  body {
+    min-height: 100vh;
+    margin: 0;
+    scroll-behavior: smooth;
+  }
+
+  /*
+    * Reset top margins on titles that are first-child.
+  */
+  h1,
+  h2,
+  h3 {
+    color: var(--color-opposite);
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export const Title = styled.h1`
+  color: blue;
+`;
